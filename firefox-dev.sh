b3428d846d5471d2af178d6083a452c1fb168f98
icebowl <craig.coleman@ksd.org>
2019-06-19 09:40:31 -0700

    AA

--- firefox-dev.sh
+++ firefox-dev.sh
@@ -1,6 +1,6 @@
 !#/bin/bash
-rm '?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US' ;
 curl -LO "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US";
 tar -xvf '?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US';
 #sudo rm -fR /opt/firefox-dev/;
 sudo mv firefox/ /opt/firefox-dev;
+rm '?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US' ;
