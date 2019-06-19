dcdfecad48f00a25fcdbedd68c108231b6be47c6
icebowl <craig.coleman@ksd.org>
2019-06-19 12:16:43 -0700

    AA

---
+++ desktop-firefox-dev.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+echo "Copying firefox-dev.png to /usr/share/pixmap/ and firefox-dev.desktop to /usr/share/applications/"
+sudo cp firefox-dev.png /usr/share/pixmaps/;
+sudo cp firefox-dev.desktop /usr/share/applications/;
+echo "* * * * * * * * * * *";
+echo " LOGOUT FOR CHANGES TO TAKE EFFECT"; 
--- firefox-dev.sh
+++ firefox-dev.sh
@@ -1,6 +1,17 @@
-!#/bin/bash
-curl -LO "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US";
-tar -xvf '?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US';
-#sudo rm -fR /opt/firefox-dev/;
-sudo mv firefox/ /opt/firefox-dev;
-rm '?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US' ;
+#!/bin/bash
+# -d "/opt/firefox-dev" ] && echo "Directory /opt/firefox-dir exists."
+if [ -d "/opt/firefox-dev"  ] 
+then
+    echo "Directory /opt/firefox-dev exists." ;
+	curl -LO "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US";
+	tar -xvf '?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US';
+	sudo rm -fR /opt/firefox-dev/;
+	sudo mv firefox/ /opt/firefox-dev;
+	rm '?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US' ;
+else
+    echo "Error: Directory /opt/firefox-dev does not exists.";
+	curl -LO "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US";
+	tar -xvf '?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US';
+	sudo mv firefox/ /opt/firefox-dev;
+	rm '?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US' ;
+fi
