eb4866f565f38248dc30ace24b7e4bad7fc62113
Tri-Tech <craig.coleman@ksd.org>
2021-09-10 05:02:52 -0700

    Add files via upload

--- set-bash_aliases.sh
+++ set-bash_aliases.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #  For the "set-bash_aliases.sh" script to work 
 #  the file bash_aliases need to be in the same directory 
-cp bash_aliases ~/.bash_aliases;
-source ~/.bash_aliases;
-ll;
+# after you run this script by typing ./set-bash_aliases.sh
+# Type source ~/.bash_aliases
+cp bash_aliases ~/.bash_aliases
